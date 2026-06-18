0d1d6e5f95543dc611f9f820059b8c502d4fc83e
tanishbhandari286 <tanishbhandari91@gmail.com>
2026-06-18 19:37:04 +0530

    did something with ghostty

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,18 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Enable OSC 52 clipboard support for SSH sessions
--- This allows yanking to work across SSH by sending clipboard data through terminal escape sequences
-if vim.fn.has("nvim") == 1 then
-  vim.g.clipboard = {
-    name = "osc52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
-  }
-end
+-- Clipboard configuration with OSC 52 support for SSH
+-- Works with Ghostty which supports OSC 52
+local osc52 = require("vim.ui.clipboard.osc52")
+
+vim.g.clipboard = {
+  name = "osc52",
+  copy = {
+    ["+"] = osc52.copy("+"),
+    ["*"] = osc52.copy("*"),
+  },
+  paste = {
+    ["+"] = osc52.paste("+"),
+    ["*"] = osc52.paste("*"),
+  },
+}
+
+vim.opt.clipboard = "unnamedplus"
