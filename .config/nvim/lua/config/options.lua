7c6ac9d93bba426b4bc550209c85a6e0a6363acc
tanishbhandari286 <tanishbhandari91@gmail.com>
2026-06-18 17:29:19 +0530

    made changes to ghostty , nvim and brewfile

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,19 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable OSC 52 clipboard support for SSH sessions
+-- This allows yanking to work across SSH by sending clipboard data through terminal escape sequences
+if vim.fn.has("nvim") == 1 then
+  vim.g.clipboard = {
+    name = "osc52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
